66eb3048f34c94f1e33e544f69d9556f0b4a146b
Bibek Ghimire <bibek@stripe.com>
2020-12-02 14:21:54 -0800

    Add SetupIntent-related typedefs

--- types/terminal.d.ts
+++ types/terminal.d.ts
@@ -2,6 +2,7 @@ import {
   IActivateTerminalRequest,
   IErrorResponse,
   IPaymentIntent,
+  ISetupIntent,
   IPaymentMethod,
   IPaymentMethodReadReusableResponse,
   IRefundChargeRequest,
@@ -221,6 +222,18 @@ export class Terminal {
         payment_method: IPaymentMethodReadReusableResponse;
       }
   >;
+
+  collectSetupIntentPaymentMethod(
+    clientSecret: string
+  ): Promise<ErrorResponse | {setupIntent: ISetupIntent}>;
+
+  cancelCollectSetupIntentPaymentMethod(): Promise<ErrorResponse | {}>;
+
+  confirmSetupIntent(
+    setupIntent: ISetupIntent,
+    customerMandateCollected: boolean
+  ): Promise<ErrorResponse | {setupIntent: ISetupIntent}>;
+
   collectRefundPaymentMethod(
     charge_id: string,
     amount: number,
